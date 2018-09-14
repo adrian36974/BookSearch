709c91c6256cd900cee921dfa8b03bca14f19719
adrian36974 <al02813163@tecmilenio.mx>
2018-09-13 19:00:35 -0500

    Initial commit

---
+++ app/src/main/java/com/adrian/tecmilenio/booksearch/activities/BookListActivity.java
@@ -0,0 +1,153 @@
+package com.adrian.tecmilenio.booksearch.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.support.v7.widget.SearchView;
+import android.app.SearchManager;
+import android.widget.SearchView.OnQueryTextListener;
+import android.widget.Toast;
+
+import com.adrian.tecmilenio.booksearch.R;
+import com.adrian.tecmilenio.booksearch.adapter.BookAdapter;
+import com.adrian.tecmilenio.booksearch.api.BookClient;
+import com.adrian.tecmilenio.booksearch.models.Book;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class BookListActivity extends AppCompatActivity {
+    public static final String BOOK_DETAIL_KEY = "book";
+    private ListView lvBooks;
+    private BookAdapter bookAdapter;
+    private BookClient client;
+    private ProgressBar progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+        lvBooks = (ListView) findViewById(R.id.lvBooks);
+        ArrayList<Book> aBooks = new ArrayList<>();
+        // initialize the adapter
+        bookAdapter = new BookAdapter(this, aBooks);
+        // attach the adapter to the ListView
+        lvBooks.setAdapter(bookAdapter);
+        progress = (ProgressBar) findViewById(R.id.progress);
+        setupBookSelectedListener();
+    }
+
+    public void setupBookSelectedListener() {
+        lvBooks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Launch the detail view passing book as an extra
+                Intent intent = new Intent(BookListActivity.this, BookDetailActivity.class);
+                intent.putExtra(BOOK_DETAIL_KEY, bookAdapter.getItem(position));
+                startActivity(intent);
+            }
+        });
+    }
+
+    // Executes an API call to the OpenLibrary search endpoint, parses the results
+    // Converts them into an array of book objects and adds them to the adapter
+    private void fetchBooks(String query) {
+        // Show progress bar before making network request
+        progress.setVisibility(ProgressBar.VISIBLE);
+        client = new BookClient();
+        client.getBooks(query, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    // hide progress bar
+                    progress.setVisibility(ProgressBar.GONE);
+                    JSONArray docs = null;
+                    if(response != null) {
+                        // Get the docs json array
+                        docs = response.getJSONArray("docs");
+                        // Parse json array into array of model objects
+                        final ArrayList<Book> books = Book.fromJson(docs);
+                        // Remove all books from the adapter
+                        bookAdapter.clear();
+                        // Load model objects into the adapter
+                        if (books.isEmpty()) {
+                            Toast.makeText(BookListActivity.this, "No se encontro ningún libro.", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(BookListActivity.this, "Se encontraron " + books.size() + " libros.", Toast.LENGTH_SHORT).show();
+                            for (Book book : books) {
+                                bookAdapter.add(book); // add book through the adapter
+                            }
+                        }
+                        bookAdapter.notifyDataSetChanged();
+                    }
+                } catch (JSONException e) {
+                    // Invalid JSON format, show appropriate error.
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Toast.makeText(BookListActivity.this, "No se encontro ningún libro.", Toast.LENGTH_SHORT).show();
+                progress.setVisibility(ProgressBar.GONE);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_book_list, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Fetch the data remotely
+                fetchBooks(query);
+                // Reset SearchView
+                searchView.clearFocus();
+                searchView.setQuery("", false);
+                searchView.setIconified(true);
+                searchItem.collapseActionView();
+                // Set activity title to search query
+                BookListActivity.this.setTitle(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
